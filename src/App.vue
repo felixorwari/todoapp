ed7a8b324d8868395481ec35a122936aed4af5be
Felix Orwari <felix.orwari@gmail.com>
2024-05-23 14:35:55 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="md:container md:mx-auto px-4 md:px-6">
+      <h1>My To Do List</h1>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
