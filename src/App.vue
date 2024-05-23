fcbf50606b1e5b98dbbde0a17094eff7223b0794
Felix Orwari <felix.orwari@gmail.com>
2024-05-23 22:19:11 +0300

    Built app navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import CalendarIcon from './components/Icons/CalendarIcon.vue'
+import TasksIcon from './components/Icons/TasksIcon.vue'
+import SettingsIcon from './components/Icons/SettingsIcon.vue'
 </script>
 
 <template>
-  <header>
-    <div class="md:container md:mx-auto px-4 md:px-6">
-      <h1>My To Do List</h1>
+  <header class="sticky top-0 z-50">
+    <div
+      class="w-full max-w-screen-xl px-4 mx-auto border-b border-gray-200 shadow-2xl shadow-gray-200 sm:shadow-none bg-white/80 backdrop-blur"
+    >
+      <nav class="flex items-center gap-8 pt-4 text-gray-500 max-w-fit">
+        <RouterLink
+          :to="{ name: 'tasks' }"
+          exactActiveClass="text-orange-600 border-orange-600"
+          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-orange-500"
+        >
+          <TasksIcon class="inline-block w-4 h-4 me-3" />
+          Tasks
+        </RouterLink>
+        <RouterLink
+          :to="{ name: 'calendar' }"
+          exactActiveClass="text-orange-600 border-orange-600"
+          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-orange-500"
+        >
+          <CalendarIcon class="inline-block w-4 h-4 me-3" />
+          Calendar
+        </RouterLink>
+        <RouterLink
+          :to="{ name: 'settings' }"
+          exactActiveClass="text-orange-600 border-orange-600"
+          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-orange-500"
+        >
+          <SettingsIcon class="inline-block w-4 h-4 me-3" />
+          Settings
+        </RouterLink>
+      </nav>
     </div>
   </header>
 
