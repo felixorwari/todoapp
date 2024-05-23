8dafbe022be07c4341a9adc7632f5d3f413114f4
Felix Orwari <felix.orwari@gmail.com>
2024-05-24 00:09:11 +0300

    Updated layout template

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,31 @@ import SettingsIcon from './components/Icons/SettingsIcon.vue'
 </script>
 
 <template>
-  <header class="sticky top-0 z-50 border-b border-gray-200 shadow-2xl shadow-gray-200">
+  <header class="sticky top-0 z-50 w-full border-b border-gray-200 shadow-xl shadow-gray-50">
     <div class="w-full max-w-screen-xl px-4 mx-auto bg-white/80 backdrop-blur">
-      <nav class="flex items-center gap-8 pt-4 text-gray-500 max-w-fit">
+      <nav class="flex items-center gap-8 pt-4 text-gray-500">
         <RouterLink
           :to="{ name: 'tasks' }"
-          exactActiveClass="text-orange-600 border-orange-600"
-          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-orange-500"
+          exactActiveClass="text-indigo-600 border-indigo-600"
+          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-indigo-500"
         >
           <TasksIcon class="inline-block w-4 h-4 me-3" />
           Tasks
         </RouterLink>
+
         <RouterLink
           :to="{ name: 'calendar' }"
-          exactActiveClass="text-orange-600 border-orange-600"
-          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-orange-500"
+          exactActiveClass="text-indigo-600 border-indigo-600"
+          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-indigo-500"
         >
           <CalendarIcon class="inline-block w-4 h-4 me-3" />
           Calendar
         </RouterLink>
+
         <RouterLink
           :to="{ name: 'settings' }"
-          exactActiveClass="text-orange-600 border-orange-600"
-          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-orange-500"
+          exactActiveClass="text-indigo-600 border-indigo-600"
+          class="flex items-center px-2 py-4 font-semibold transition-all border-b-4 border-transparent hover:border-indigo-500"
         >
           <SettingsIcon class="inline-block w-4 h-4 me-3" />
           Settings
